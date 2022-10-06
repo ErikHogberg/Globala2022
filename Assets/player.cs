ac1008ef791034bb12f1275fc3efa2847925866f
Erik Högberg <erikh_93@hotmail.com>
2022-10-06 10:31:55 +0200

    player movement

--- Assets/player.cs
+++ Assets/player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    public float speed = 10f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal");
+        Vector3 pos = transform.position;
+        pos.x += x * Time.deltaTime * speed;
+        transform.position = pos;
     }
 }
